77bd0afaed0a7f19abc88e6bbaf111ad57905466
Pradeep Chauhan <80189185+DeepCoderr@users.noreply.github.com>
2021-10-28 09:08:12 +0530

    Update bomb.sh

--- bomb.sh
+++ bomb.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 #!/bin/bash
 # Author : Deep
-# Copyright (c)
 # Script follows here:
 
-echo "What is your name?"
-read PERSON
-echo "Hello, $PERSON"
+echo "working"
+sh /XLR8_BOMBER/xlr8.sh
